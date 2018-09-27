513ec74bdf240015a618cfd0b39557076e3f18af
jakobst1n <jakob.stendahl@outlook.com>
2018-09-27 09:22:03 +0200

    :hankey: Workaround for not getting the python lib to run as non-root

--- bin/luxcena-neo.sh
+++ bin/luxcena-neo.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-runuser -l pi -c "export NODE_ENV=production; node ~/luxcena-neo-install/src/app.js"
+# We want to eventually run the server as another user than root. But right now,
+# the python script needs to be run as root, and since the server is starting them
+# the server needs root as well.
+
+#runuser -l pi -c "export NODE_ENV=production; node ~/luxcena-neo-install/src/app.js"
+export NODE_ENV=production; node /home/pi/luxcena-neo-install/src/app.js
