a5fc50bca8c5d351ac87b2334cea55388db5af09
Jakob Stendahl <jakob.stendahl@outlook.com>
2018-10-18 21:17:39 +0200

    :construction: Update install-dir-path to new user

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let io = require("socket.io").listen(server);
 
 // Firstly we set up all globals, check that the usrData dir exists, if not, we run the setup
 let srcDir = __dirname;
-let installDir = "/home/pi/luxcena-neo-install/"
+let installDir = "/home/lux-neo/install/"
 if (process.argv.length >= 3) { installDir = process.argv[2]; }
 let dataDir = installDir + "/userdata/";
 if (!fse.existsSync(dataDir)) { throw new Error("APPDIR not found! Exiting..."); }
